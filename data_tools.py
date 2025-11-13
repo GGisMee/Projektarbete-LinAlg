b314e83e2a55dff571394695a56c774720a9cb27
abbnoamar <Noah.Marklund@abbgymnasiet.se>
2025-11-13 21:16:39 +0100

    comments

--- data_tools.py
+++ data_tools.py
@@ -11,8 +11,8 @@ import csv
 #    path = "Data/Jury/2023_jury_results.csv"
 #    votes = CSVTools.load(path)
 #
-#    # Konvertera till matris.
-#    matrix, countries = MatrixTools.matrix_from_data(votes)
+#    # Konvertera till matris (storlek 5x5)
+#    matrix, countries = MatrixTools.matrix_from_data(votes, 5)
 #
 #    # Printa (frivilligt)
 #    print(matrix)
@@ -34,21 +34,24 @@ keys_to_remove = ['Contestant', 'Total score', 'Jury score', 'Televoting score']
 
 class MatrixTools:    
     @staticmethod
+    # Creates a matrix from the given csv data.
+    # Set max_size = 0 for a matrix with data for every country.
     def matrix_from_data(data, max_size: int = 0, filter = None):
         max_size = sys.maxsize if max_size == 0 else max_size
         
-        # Get the order of the country (by row)
+        # Get ordered country keys
         ordered_keys = MatrixTools.__get_country_order(data, max_size)
         
         # Strip data
         countries = MatrixTools.__strip_datas(data, max_size)
         
-        # Permutate collumns to match up countries between row/col positions
+        # Permutate collumns
         countries = MatrixTools.__permutate_collumns_by_keys(countries, ordered_keys)
         
-        # Convert to np matrix
+        # Convert to matrix
         return MatrixTools.__dict_to_matrix(countries, ordered_keys, filter)
     
+    # Get the order of the countries (by row)
     @staticmethod
     def __get_country_order(countries, max_size: int):
         ordered_keys = []
@@ -59,13 +62,14 @@ class MatrixTools:
             if contestant is not None:
                 ordered_keys.append(contestant)
         return ordered_keys
-                    
+    
+    # Move collumns by key to the permutation_keys' positions
     @staticmethod
-    def __permutate_collumns_by_keys(countries, permutation_key):
+    def __permutate_collumns_by_keys(countries, permutation_keys):
         permutated = []
         for country in countries:
             new_country = {}
-            for key in permutation_key:
+            for key in permutation_keys:
                 if key in country:
                     new_country[key] = country[key]
             permutated.append(new_country)
@@ -90,7 +94,7 @@ class MatrixTools:
             stripped_countries.append(MatrixTools.__strip_data(country, max_size))
         return stripped_countries
 
-    # Convert dict data to a numeric np matrix
+    # Convert dict data to np matrix
     @staticmethod
     def __dict_to_matrix(countries, ordered_keys, filter = None):
         if filter == None:
