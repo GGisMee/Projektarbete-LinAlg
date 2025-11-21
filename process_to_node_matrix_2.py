44389d707e968c8dc7de729abc0208d796557314
steina <steina@DESKTOP-30N9JIE.localdomain>
2025-11-21 15:25:38 +0100

    added function to convert matris into binary matrix

---
+++ process_to_node_matrix_2.py
@@ -0,0 +1,25 @@
+import numpy as np
+from data_tools import CSVTools, MatrixTools
+
+def get_binary_matrix(threshold=8, max_size=0):
+    
+    path = "Data/Jury/2023_jury_results.csv"
+    votes = CSVTools.load(path)
+    
+    matrix, countries = MatrixTools.matrix_from_data(votes, max_size)
+
+    binary_list = []
+
+    for row in matrix:
+        new_row = []
+        for score in row:
+            if score >= threshold:
+                new_row.append(1)  
+            else:
+                new_row.append(0) 
+        
+        binary_list.append(new_row)
+
+    binary_matrix = np.array(binary_list)
+
+    return binary_matrix, countries
