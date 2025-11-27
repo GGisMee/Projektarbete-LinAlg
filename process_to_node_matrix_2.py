2d4254dceeaba310f4825c85b4a680a991498214
gustavgamstedt <gustav.gamstedt@outlook.com>
2025-11-27 10:18:07 +0100

    lade till ex för 2

--- process_to_node_matrix_2.py
+++ process_to_node_matrix_2.py
@@ -7,9 +7,7 @@ def get_binary_matrix(threshold=8, max_size=0):
     votes = CSVTools.load(path)
     
     matrix, countries = MatrixTools.matrix_from_data(votes, max_size)
-
     binary_list = []
-
     for row in matrix:
         new_row = []
         for score in row:
@@ -23,3 +21,7 @@ def get_binary_matrix(threshold=8, max_size=0):
     binary_matrix = np.array(binary_list)
 
     return binary_matrix, countries
+
+# Kort exempel för denna fil
+if __name__ == "__main__":
+    get_binary_matrix()
