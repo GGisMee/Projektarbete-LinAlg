e69070a822ec5b4c9c594d6c676394b05160d326
gustavgamstedt <gustav.gamstedt@outlook.com>
2025-11-28 10:57:46 +0100

    modified 2 to work simply with 1

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import data_tools_1
-import process_to_node_matrix_2
-import find_indices_cliques_3
+import data_tools_1 as import_tools
+import process_to_node_matrix_2 as to_binary
+import find_indices_cliques_3 as find_cliques
 import partition_cliques_4
 import rank_cliques_by_size_5
 import show_cliques_6
-import compare_data_7
+# import compare_data_7
 
 
 if __name__ == "__main__":
-    pass
+    number_of_countries = 10
+
+    files = ("Data/Jury/2016_jury_results.csv", "Data/Jury/2017_jury_results.csv")
+    csv_tools = import_tools.CSVTools()
+    votes = csv_tools.load(relative_path=files[0])
+    matrix_tools = import_tools.MatrixTools()
+    matrix, countries = matrix_tools.matrix_from_data(votes, number_of_countries)
+
+    binary_matrix = to_binary.get_binary_matrix(matrix, threshold=6)
+    
--- process_to_node_matrix_2.py
+++ process_to_node_matrix_2.py
@@ -1,12 +1,8 @@
 import numpy as np
 from data_tools_1 import CSVTools, MatrixTools
 
-def get_binary_matrix(threshold=8, max_size=0):
+def get_binary_matrix(matrix: np.ndarray, threshold:int=8) -> np.ndarray:
     
-    path = "Data/Jury/2023_jury_results.csv"
-    votes = CSVTools.load(path)
-    
-    matrix, countries = MatrixTools.matrix_from_data(votes, max_size)
     binary_list = []
     for row in matrix:
         new_row = []
@@ -20,8 +16,13 @@ def get_binary_matrix(threshold=8, max_size=0):
 
     binary_matrix = np.array(binary_list)
 
-    return binary_matrix, countries
+    return binary_matrix
 
 # Kort exempel för denna fil
 if __name__ == "__main__":
-    get_binary_matrix()
+    path = "Data/Jury/2023_jury_results.csv"
+    votes = CSVTools.load(path)
+    
+    matrix, countries = MatrixTools.matrix_from_data(votes, 10)
+    binary_matrix = get_binary_matrix(matrix, 6)
+    print(binary_matrix)
